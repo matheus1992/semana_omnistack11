16c5917528d154b8628afe8f85462d884db2d198
Matheus Nasicmento <matheus.bellio.nascimento@gmail.com>
2020-03-23 20:59:15 -0300

    Aula 1 23-03

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({'evento' : 'Semana OmniStack 11.0', 'aluno' : 'Matheus Nascimento'});
+});
+
+app.listen(3333);
