205a94281514f7c10fada3eb69216a7a50aad685
Matheus Nasicmento <matheus.bellio.nascimento@gmail.com>
2020-03-28 00:51:27 -0300

    Aula 5- Teste

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+
+const { errors } = require('celebrate');
+
 const routes = require('./routes');
 
 const app = express();
@@ -8,7 +11,7 @@ app.use(cors());
 
 //CONVERTE CORPO DAS REQUISIÇÕES PARA JSON
 app.use(express.json());
-
 app.use(routes);
+app.use(errors());
 
-app.listen(3333);
+module.exports = app;
